663b7612b01171e6d74e565d8f25ef8bd44dc383
Max Furtado <maximilianomfurtado@gmail.com>
2025-03-26 19:25:18 -0300

    § o SUBNET PUBLIC foi adicionado

--- main.tf
+++ main.tf
@@ -41,3 +41,31 @@ resource "aws_subnet" "sn_pub02" {
     Name = "elvintao-sn_pub02"
   }
 }
+
+# SUBNET PUBLIC 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.elvintao_vpc.id
+  tags = {
+    Name = "elvintao-igw" 
+  }
+}
+
+resource "aws_route_table" "route_pub" {
+  vpc_id = aws_vpc.elvintao_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "elvintao-routetable"
+  }
+}
+
+resource "aws_route_table_association" "pub01assoc" {
+  subnet_id = aws_subnet.sn_pub01.id
+  route_table_id = aws_route_table.route_pub.id
+}
+resource "aws_route_table_association" "pub02assoc" {
+  subnet_id = aws_subnet.sn_pub02.id
+  route_table_id = aws_route_table.route_pub.id
+}
