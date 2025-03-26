0002b82f8a61f08e906fd527149c4877fee0323e
Elvyngthon <el.cayro@gmail.com>
2025-03-26 15:24:31 -0300

    § modificação do ip

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "elvintao_vpc" {
-  cidr_block = "172.200.0.0/16"
+  cidr_block = "172.102.0.0/16"
   tags = {
     Name = "elvintao-vpc"
   }
@@ -11,7 +11,7 @@ resource "aws_vpc" "elvintao_vpc" {
 
 resource "aws_subnet" "sn_priv01" {
   vpc_id = aws_vpc.elvintao_vpc.id
-  cidr_block = "172.200.1.0/24"
+  cidr_block = "172.102.1.0/24"
   availability_zone = "us-west-1c"
   tags = {
     Name = "elvintao-sn_priv01"
@@ -19,7 +19,7 @@ resource "aws_subnet" "sn_priv01" {
 }
 resource "aws_subnet" "sn_priv02" {
   vpc_id = aws_vpc.elvintao_vpc.id
-  cidr_block = "172.200.2.0/24"
+  cidr_block = "172.102.2.0/24"
   availability_zone = "us-west-1b"
   tags = {
     Name = "elvintao-sn_priv02"
@@ -27,7 +27,7 @@ resource "aws_subnet" "sn_priv02" {
 }
 resource "aws_subnet" "sn_pub01" {
   vpc_id = aws_vpc.elvintao_vpc.id
-  cidr_block = "172.200.3.0/24"
+  cidr_block = "172.102.3.0/24"
   availability_zone = "us-west-1c"
   tags = {
     Name = "elvintao-sn_pub01"
@@ -35,7 +35,7 @@ resource "aws_subnet" "sn_pub01" {
 }
 resource "aws_subnet" "sn_pub02" {
   vpc_id = aws_vpc.elvintao_vpc.id
-  cidr_block = "172.200.4.0/24"
+  cidr_block = "172.102.4.0/24"
   availability_zone = "us-west-1b"
   tags = {
     Name = "elvintao-sn_pub02"
