792b2c1089d4de9a8bc1652d6680d0301023fe85
Leonardo Cella <leeoceella@gmail.com>
2025-03-28 21:41:06 -0300

    § o main do project ganhou os modules backend e frontend.

--- project/main.tf
+++ project/main.tf
@@ -23,3 +23,15 @@ module "loadbalancer" {
     sn_pub01 = module.vpc.sn_pub01
     sn_pub02 = module.vpc.sn_pub02
 }
+
+module "frontend" {
+    source = "./modules/frontend"
+    vpc_id = module.vpc.vpc_id
+    sn_pub01 = module.vpc.sn_priv01
+}
+
+module "backend" {
+    source = "./modules/backend"
+    vpc_id = module.vpc.vpc_id
+    sn_pub01 = module.vpc.sn_priv02
+}
