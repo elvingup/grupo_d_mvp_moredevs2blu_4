4c5f2cc7ad32dfb7c991b000cbb6ca66f3f16925
Cursos Oficiais Microsoft <moc@proway.treina>
2025-03-31 19:33:53 -0300

    FIX § a region da structure ganhou um default.

--- structure/variables.tf
+++ structure/variables.tf
@@ -1,9 +1,11 @@
 # Este arquivo eh responsavel por definir as variaveis que serao usadas no modulo de infraestrutura como o nome do projeto e a regiao da AWS onde a infra sera criada.
 
 variable "project_region" {
+    default = "us-west-1"
     type = string
 }
 
 variable "project_name" {
+    default="elvintao"
     type=string
 }
