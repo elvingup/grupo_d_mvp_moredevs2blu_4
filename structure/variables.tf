e7344d1a3d756214e6b8c64f1fa9f9778b416c6c
Elvyngthon <el.cayro@gmail.com>
2025-04-01 14:46:22 -0300

    § structure foi concluido

--- structure/variables.tf
+++ structure/variables.tf
@@ -1,4 +1,5 @@
 # Este arquivo eh responsavel por definir as variaveis que serao usadas no modulo de infraestrutura como o nome do projeto e a regiao da AWS onde a infra sera criada.
+# o terraform apply vai exigir que o usuario informe os valores respectivos a cada variavel.
 
 variable "project_region" {
     type = string
