3d1e81db9366f0d2178beb9c0558ea57cc9bcc64
Elvyngthon <el.cayro@gmail.com>
2025-04-01 16:04:31 -0300

    FEAT § vpc/main foi atualizado

--- project/modules/vpc/main.tf
+++ project/modules/vpc/main.tf
@@ -5,7 +5,7 @@
 resource "aws_vpc" "elvintao_vpc" {
   cidr_block = "172.102.0.0/16"
   tags = {
-    Name = "elvintao_vpc"
+    Name = "${var.project_name}_vpc"
   }
 }
 
@@ -13,7 +13,7 @@ resource "aws_vpc" "elvintao_vpc" {
 resource "aws_subnet" "sn_priv01" {
   vpc_id = aws_vpc.elvintao_vpc.id
   cidr_block = "172.102.1.0/24"
-  availability_zone = "${var.project_region}c"
+  availability_zone = "us-west-1c"
   tags = {
     Name = "${var.project_name}-sn_priv01"
   }
@@ -25,7 +25,7 @@ resource "aws_subnet" "sn_priv01" {
 resource "aws_subnet" "sn_priv02" {
   vpc_id = aws_vpc.elvintao_vpc.id
   cidr_block = "172.102.2.0/24"
-  availability_zone = "${var.project_region}b"
+  availability_zone = "us-west-1b"
   tags = {
     Name = "${var.project_name}-sn_priv02"
   }
@@ -37,7 +37,7 @@ resource "aws_subnet" "sn_priv02" {
 resource "aws_subnet" "sn_pub01" {
   vpc_id = aws_vpc.elvintao_vpc.id
   cidr_block = "172.102.3.0/24"
-  availability_zone = "${var.project_region}c"
+  availability_zone = "us-west-1c"
   tags = {
     Name = "${var.project_name}-sn_pub01"
   }
@@ -49,8 +49,37 @@ resource "aws_subnet" "sn_pub01" {
 resource "aws_subnet" "sn_pub02" {
   vpc_id = aws_vpc.elvintao_vpc.id
   cidr_block = "172.102.4.0/24"
-  availability_zone = "${var.project_region}b"
+  availability_zone = "us-west-1b"
   tags = {
     Name = "${var.project_name}-sn_pub02"
   }
+}
+
+# INTERNET GATEWAY E ROUTE TABLE 
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.elvintao_vpc.id
+  tags = {
+    Name = "elvintao-igw" 
+  }
+}
+
+resource "aws_route_table" "route_pub" {
+  vpc_id = aws_vpc.elvintao_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "elvintao-routetable"
+  }
+}
+
+resource "aws_route_table_association" "pub01assoc" {
+  subnet_id = aws_subnet.sn_pub01.id
+  route_table_id = aws_route_table.route_pub.id
+}
+resource "aws_route_table_association" "pub02assoc" {
+  subnet_id = aws_subnet.sn_pub02.id
+  route_table_id = aws_route_table.route_pub.id
 }
